227324da5f9272f05aba46188841a9781d0be68e
hotnessOverload <valery.lebedikova@gmail.com>
2018-09-25 00:57:00 +0300

    String Demolish

--- MomentaryLapseOfReason.py
+++ MomentaryLapseOfReason.py
@@ -14,32 +14,20 @@ for i in range(d):
   elif x % 2 == 0:
     k, even = k+k13, even+1
   else:
-    k, odd = k+k26, odd+1
+    kcmcmd, odd = k+k26, odd+1
 for i in range(d,n):
   # элемент удаляется из очереди
-  if queue[i % d] % 13 == 0:
-    if queue[i % d] % 2 == 0:
-      k26, even = k26-1, even-1
-    else:
-      k13, odd = k13-1, odd-1
+
   elif queue[i % d] % 2 == 0:
     even -= 1
   else:
     odd -= 1
-  # вводим элемент
-  x = int(input("x"))
-  if x % 13 == 0:
-    if x % 2 == 0:
-      k += odd
-      k26, even = k26+1, even+1
-    else:
-      k += even
-      k13, odd = k13+1, odd+1
   elif x % 2 == 0:
     k, even = k+k13, even+1
   else:
     k, odd = k+k26, odd+1
   queue[i % d] = x
 print(k)
+
 
 
